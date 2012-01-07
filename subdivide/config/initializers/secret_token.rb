7bb94b415aeafc95041226ade84951d076549c3f
Michael Chang <thenewme91@gmail.com>
2012-01-07 04:11:17 +0000

    HW/MC - Initial commit.

---
+++ subdivide/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subdivide::Application.config.secret_token = '1879772a1ebad19cbc77536e980405b158bed3702f47efb80230768be4e536d34906dc2211faeb51797ff37f85074b45923a34944cd518acd62b5f725f9786b8'
