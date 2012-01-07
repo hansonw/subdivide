ca39f5ada6555d73ab6879833a7f20043370d2d0
Michael Chang <m9chang@uwaterloo.ca>
2012-01-07 18:16:41 +0000

    Stub out videos#index

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,17 @@
 class VideosController < ApplicationController
+  def index
+    @videos = Video.all
+    respond_to do |format|
+      format.html # index.html.slim
+      format.xml  { render :xml => @videos }
+      format.json { render :json => @videos }
+    end
+  end
+# index
+# new
+# create
+# show
+# edit
+# update
+# destroy
 end
