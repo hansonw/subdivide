0e0dbbf548645d30fab90a35a1cdef92d7db3ff6
Michael Chang <m9chang@uwaterloo.ca>
2012-01-08 19:11:50 +0000

    Fix validators in time point model

--- app/models/time_point.rb
+++ app/models/time_point.rb
@@ -8,9 +8,11 @@ class TimePoint < ActiveRecord::Base
   def end_must_follow_start
     if time_point_type == 1
       prev_tp = TimePoint.where(['cast(time as double precision) <= ?', time.to_f])
-                         .where(id.nil? ? ['id != ?', id] : '1 = 1')
+                         .where(id.nil? ? '1 = 1' : ['id != ?', id])
                          .last(:conditions => {:voice => voice, :video_id => video_id})
-      if (prev_tp.nil?) && (prev_tp.time_point_type == 1)
+      y 'prev'
+      y prev_tp
+      if (prev_tp.nil? == false) && (prev_tp.time_point_type == 1)
         errors.add(:time_point_type,'end point cannot be inserted after an end point')
       end
     end
@@ -19,9 +21,11 @@ class TimePoint < ActiveRecord::Base
   def end_must_not_preceed_end
     if time_point_type == 1
       next_tp = TimePoint.where(['cast(time as double precision) >= ?', time.to_f])
-                         .where(id.nil? ? ['id != ?', id] : '1 = 1')
+                         .where(id.nil? ? '1 = 1' : ['id != ?', id])
                          .first(:conditions => {:voice => voice, :video_id => video_id})
-      if (next_tp.nil?) && (next_tp.time_point_type == 1)
+      y 'next'
+      y next_tp
+      if (next_tp.nil? == false) && (next_tp.time_point_type == 1)
         errors.add(:time_point_type,'end point cannot be inserted before an end point')
       end
     end
