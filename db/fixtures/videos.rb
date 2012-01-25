a52b6a3365c50ee89403431b7f89b87db7f39cd0
Michael Chang <m9chang@uwaterloo.ca>
2012-01-25 05:12:55 +0000

    Add new video for translations functionality.

--- db/fixtures/videos.rb
+++ db/fixtures/videos.rb
@@ -1,5 +1,21 @@
+# English
 Video.seed do |v|
   v.id  = 2
   v.url = "//iterate.ca/video.webm"
   v.uuid = UUIDTools::UUID.random_create.to_s
 end
+
+# French
+Video.seed do |v|
+  v.id  = 3
+  v.url = "//iterate.ca/video.webm"
+  v.uuid = UUIDTools::UUID.random_create.to_s
+end
+
+# MP4
+Video.seed do |v|
+  v.id  = 4
+  v.url = "//iterate.ca/video.mp4"
+  v.uuid = UUIDTools::UUID.random_create.to_s
+end
+
