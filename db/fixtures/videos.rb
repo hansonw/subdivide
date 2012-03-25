d29219ee5773e43f21c6fde74848926c3500f57a
Michael Chang <m9chang@uwaterloo.ca>
2012-03-25 21:31:27 +0000

    Update fixture to remove redundant entry.

--- db/fixtures/videos.rb
+++ db/fixtures/videos.rb
@@ -1,20 +1,13 @@
-# English
+# WEBM
 Video.seed do |v|
   v.id  = 2
   v.url = "//iterate.ca/video.webm"
   v.uuid = UUIDTools::UUID.random_create.to_s
 end
 
-# French
-Video.seed do |v|
-  v.id  = 3
-  v.url = "//iterate.ca/video.webm"
-  v.uuid = UUIDTools::UUID.random_create.to_s
-end
-
 # MP4
 Video.seed do |v|
-  v.id  = 4
+  v.id  = 3
   v.url = "//iterate.ca/video.mp4"
   v.uuid = UUIDTools::UUID.random_create.to_s
 end
