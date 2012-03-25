30287bffca1c0018c0685e3f08a6b0e876b04bbc
Michael Chang <m9chang@uwaterloo.ca>
2012-03-25 21:48:31 +0000

    Add fixture for track sets.

---
+++ db/fixtures/track_sets.rb
@@ -0,0 +1,23 @@
+TrackSet.seed do |ts|
+  ts.id = 1
+  ts.title = "English"
+  ts.video_id = 1
+end
+
+TrackSet.seed do |ts|
+  ts.id = 2
+  ts.title = "French"
+  ts.video_id = 1
+end
+
+TrackSet.seed do |ts|
+  ts.id = 3
+  ts.title = "English"
+  ts.video_id = 2
+end
+
+TrackSet.seed do |ts|
+  ts.id = 4
+  ts.title = "French"
+  ts.video_id = 2
+end
