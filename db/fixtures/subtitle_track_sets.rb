982f3f29fa81d33193c934efb6f9c9a4209666d4
Michael Chang <m9chang@uwaterloo.ca>
2012-03-25 21:56:38 +0000

    Typo.

--- db/fixtures/subtitle_track_sets.rb
+++ db/fixtures/subtitle_track_sets.rb
@@ -1,22 +1,22 @@
-TrackSet.seed do |ts|
+SubtitleTrackSet.seed do |ts|
   ts.id = 1
   ts.title = "English"
   ts.video_id = 1
 end
 
-TrackSet.seed do |ts|
+SubtitleTrackSet.seed do |ts|
   ts.id = 2
   ts.title = "French"
   ts.video_id = 1
 end
 
-TrackSet.seed do |ts|
+SubtitleTrackSet.seed do |ts|
   ts.id = 3
   ts.title = "English"
   ts.video_id = 2
 end
 
-TrackSet.seed do |ts|
+SubtitleTrackSet.seed do |ts|
   ts.id = 4
   ts.title = "French"
   ts.video_id = 2
