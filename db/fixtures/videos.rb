1e2238a70aa4605025d2ff2cd0ee4cf805b32aff
Michael Chang <m9chang@uwaterloo.ca>
2012-03-25 21:45:03 +0000

    Update video IDs to start from 1.

--- db/fixtures/videos.rb
+++ db/fixtures/videos.rb
@@ -1,13 +1,13 @@
 # WEBM
 Video.seed do |v|
-  v.id  = 2
+  v.id  = 1
   v.url = "//iterate.ca/video.webm"
   v.uuid = UUIDTools::UUID.random_create.to_s
 end
 
 # MP4
 Video.seed do |v|
-  v.id  = 3
+  v.id  = 2
   v.url = "//iterate.ca/video.mp4"
   v.uuid = UUIDTools::UUID.random_create.to_s
 end
