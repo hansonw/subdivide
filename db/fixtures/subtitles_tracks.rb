94f7c70beb3e49664397fff56e7c538a365f0633
Michael Chang <m9chang@uwaterloo.ca>
2012-03-28 13:32:05 +0000

    db/fixtures/subtitles_tracks.rb

--- db/fixtures/subtitles_tracks.rb
+++ db/fixtures/subtitles_tracks.rb
@@ -1,4 +1,6 @@
-Subtitle.all.each do |st|
-  st.subtitle_track_id = Video.find(st.video_id).subtitle_track_set.first.subtitle_track.first.id
-  st.save()
+ActiveRecord::Base.uncached do
+  Subtitle.all.each do |st|
+    st.subtitle_track = Video.find(st.video_id).subtitle_track_set.first.subtitle_track.first
+    st.save()
+  end
 end
